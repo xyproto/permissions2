ad844ca71631ec97f7896980ab4b3e8d39e06c24
Alexander Rødseth <alexander.rodseth@usit.uio.no>
2014-08-29 16:32:19 +0200

    Added checks

--- helperfunctions.go
+++ helperfunctions.go
@@ -1,8 +1,13 @@
 package userstate
 
 import (
+	"errors"
 	"math/rand"
 	"strings"
+)
+
+const (
+	USERNAME_ALLOWED_LETTERS = "abcdefghijklmnopqrstuvwxyzæøåABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ_0123456789"
 )
 
 // Converts "true" or "false" to a bool
@@ -69,4 +74,20 @@ func RandomCookieFriendlyString(length int) string {
 
 func CleanUserInput(val string) string {
 	return strings.Replace(val, "<", "&lt;", -1)
+}
+
+func Check(username, password, email string) error {
+NEXT:
+	for _, letter := range username {
+		for _, allowedLetter := range USERNAME_ALLOWED_LETTERS {
+			if letter == allowedLetter {
+				continue NEXT
+			}
+		}
+		return errors.New("Only a-å, A-Å, 0-9 and _ are allowed in usernames.")
+	}
+	if username == password {
+		return errors.New("Username and password must be different, try another password.")
+	}
+	return nil
 }
